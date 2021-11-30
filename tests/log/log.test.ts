3e398068b1bea325a012294f2c4213fd6e049321
Nitzan Ohana <v-nohana@commoninja.com>
2021-11-30 10:25:53 +0200

    clean log as well

--- tests/log/log.test.ts
+++
@@ -1,9 +0,0 @@
-import { loggerFactory } from '../../lib';
-
-test('LoggerFactory creates a logger', () => {
-  const logger = loggerFactory.getLogger(__filename);
-  logger.debug('debug');
-  logger.info('info');
-  logger.warn('warn');
-  logger.error('error');
-});
