2c74927f8ff6fc6a41081274d461832f7b97bd44
geiger01 <tantangeiger@gmail.com>
2022-03-28 16:14:58 +0300

    added customer type

---
+++ lib/types/customer/customer.type.ts
@@ -0,0 +1,42 @@
+import { IProduct } from "../product/product.type";
+
+export interface ICustomer {
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phoneNumber?: number;
+  acceptMarketing?: boolean;
+  address?: IAddress;
+  tags?: string[];
+  orders?: ICustomerOrder[];
+}
+
+interface IAddress {
+  zip?: string;
+  country?: string;
+  city?: string;
+  address1?: string;
+  address2?: string;
+}
+
+interface ICustomerOrder {
+  orderNumber?: string;
+  totalPrice?: number;
+  totalDiscount?: number;
+  totalTax?: number;
+  totalShipping?: number;
+  status?: 'active' | 'cancelled';
+  paymentStatus?: 'paid' | 'pending' | 'refunded';
+  createdAt?: string;
+  updatedAt?: string;
+  billingAddress?: IAddress;
+  shippingAddress?: IAddress;
+  currency?: string;
+  fulfillmentStatus?: 'fulfilled' | 'partial' | 'unfulfilled';
+  lineItems?: IProduct[];
+}
+
+
