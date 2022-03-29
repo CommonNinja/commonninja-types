3e7198afc76fe48be4c012d7ef5934f690d7b4a1
geiger01 <tantangeiger@gmail.com>
2022-03-29 17:09:28 +0300

    fix: added to IAdrdress type

--- lib/types/customer/customer.type.ts
+++ lib/types/customer/customer.type.ts
@@ -17,6 +17,8 @@ export interface ICustomer {
 export interface IAddress {
   zip?: string;
   country?: string;
+  countryCode?: string;
+  state?: string;
   city?: string;
   address1?: string;
   address2?: string;
