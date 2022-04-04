a82a318dbfb5547dc100984f9416948f27174b53
geiger01 <tantangeiger@gmail.com>
2022-04-04 14:36:17 +0300

    fix: added customer id to order interface

--- lib/types/order/order.type.ts
+++ lib/types/order/order.type.ts
@@ -4,6 +4,7 @@ import { IProduct } from '../product/product.type';
 export interface IOrder {
   orderNumber?: string;
   id?: string;
+  customerId?: string;
   totalPrice?: string;
   totalDiscount?: string;
   totalTax?: string;
