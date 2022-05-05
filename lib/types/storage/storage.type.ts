f4d27fb0b60fbae171641f835b506a73f6ab415f
Daniel Sternlicht <gdblogst@gmail.com>
2022-05-05 19:07:43 +0300

    fix: adding folder type

--- lib/types/storage/storage.type.ts
+++ lib/types/storage/storage.type.ts
@@ -12,6 +12,18 @@ export interface IBucket extends IModelWrapper {
   };
 }
 
+export interface IFolder extends IModelWrapper {
+  id?: string;
+  appId?: string;
+  platform?: TPlatform;
+  name?: string;
+  path?: string;
+  user?: {
+    platform?: TPlatform;
+    platformUserId?: string;
+  };
+}
+
 export interface IFile extends IModelWrapper {
   id?: string;
   bucket?: string;
@@ -20,6 +32,7 @@ export interface IFile extends IModelWrapper {
   contentType?: string;
   name?: string;
   size?: number;
+  folderId?: string | null;
   url?: string;
   body?: any;
   user?: {
