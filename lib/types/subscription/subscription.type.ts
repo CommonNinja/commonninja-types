e85eeede5d4a48c6eeb5fa95fc030acfe75af8c9
geiger01 <tantangeiger@gmail.com>
2022-06-08 15:25:39 +0300

    fix: added trialPeriodDays to subscription type

--- lib/types/subscription/subscription.type.ts
+++ lib/types/subscription/subscription.type.ts
@@ -1,10 +1,11 @@
 export interface ISubscription {
-  amount: number;
-  period: 'MONTH' | 'ANNUAL';
-  periodCount?: number;
-  id?: string;
-  name?: string;
-  currency?: string;
-  redirectUrl?: string;
-  isTest?: boolean;
+	amount: number;
+	period: 'MONTH' | 'ANNUAL';
+	periodCount?: number;
+	id?: string;
+	name?: string;
+	currency?: string;
+	redirectUrl?: string;
+	isTest?: boolean;
+	trialPeriodDays?: number;
 }
