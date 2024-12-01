661f7a34893e3b0146052bd09177d31fcb7da54c
Daniel Sternlicht <gdblogst@gmail.com>
2024-12-01 15:11:59 +0200

    fix: added ISubscriptionModifier

--- lib/types/subscription/subscription.type.ts
+++ lib/types/subscription/subscription.type.ts
@@ -1,8 +1,17 @@
 export type TPayloadSubscriptionStatus =
-  | 'active'
-  | 'cancelled'
-  | 'pending'
-  | 'expired'
-  | 'declined'
-  | 'trial'
-  | 'unknown';
+  | "active"
+  | "cancelled"
+  | "pending"
+  | "expired"
+  | "declined"
+  | "trial"
+  | "unknown";
+
+export interface ISubscriptionModifier {
+  id: string;
+  subscriptionId: string;
+  amount: number;
+  recurring: boolean;
+  description: string;
+  currency: string;
+}
